bf5b73724b18f13463b927e1bc70453359000b9f
Harshit Bhardwaj <harshit704892@gmail.com>
2025-10-07 18:58:27 +0530

    new update

--- que3.py
+++ que3.py
@@ -5,6 +5,15 @@ def removeDuplicate(self,nums):
     for i in range(0 ,len(nums)):
         if(nums[i] != nums[i-1]):
             k += 1
+    
+    currentIndex=1
+    for i in range(1,len(nums)):
+        if nums[i] != nums[i-1]:
+            nums[currentIndex] = nums[i]
+            currentIndex += 1
+    
+    for i in range(currentIndex,len(nums)):
+        nums[i]="_"
     return k
 
 print(removeDuplicate([3,3,3,4,5,5,5,6,7,7,8,9]))
