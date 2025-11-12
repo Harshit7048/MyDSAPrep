e210666e07b9d28efc46216c93800e52cce804f1
Harshit Bhardwaj <harshit704892@gmail.com>
2025-11-12 21:36:13 +0530

    new question done

---
+++ leetcode Questions/que344.py
@@ -0,0 +1,10 @@
+print(" question number 344 | Reverse the string")
+
+def reverse_string(inp):
+    itmes = list(inp)
+    print(itmes)
+    itmes[:]=itmes[::-1] 
+    print(itmes) 
+
+
+reverse_string("hello")
